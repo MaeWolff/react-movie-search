0e0db441757bbe04291f0bfb3b57bb476e01013b
MaevaWolff <maeva.wolff@hetic.net>
2020-11-18 12:49:17 +0100

    add theme on app

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,5 +1,25 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
+import styled, { css } from "styled-components";
+import { Button } from "../Component/index";
+
+const FlexCenter = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Layout = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  ${FlexCenter};
+`;
+
+const FormCard = styled.div`
+  width: 50%;
+  ${FlexCenter};
+`;
 
 export default function LoginPage() {
   const [username, setUsername] = useState();
@@ -12,13 +32,17 @@ export default function LoginPage() {
   }
 
   return (
-    <div>
-      Login page
-      <input
-        type="text"
-        onChange={(e) => setUsername((username) => e.target.value)}
-      />
-      <button onClick={handleConnexion}>Se connecter</button>
-    </div>
+    <Layout>
+      <FormCard>
+        <>
+          <label htmlFor="username">Inscris ton pseudo ici</label>
+          <input
+            type="text"
+            onChange={(e) => setUsername((username) => e.target.value)}
+          />
+        </>
+        <Button handleClick={handleConnexion} label="Se connecter" />
+      </FormCard>
+    </Layout>
   );
 }
---
+++ src/theme/theme.ts
@@ -0,0 +1,9 @@
+import { DefaultTheme } from "styled-components";
+
+const theme: DefaultTheme = {
+  colors: {
+    grey: "#777777",
+  },
+};
+
+export default theme;

65bbd02c2ee333048c518bf0e5bfa44e1a006141
Nstampfli <nathan.stampfli@hetic.net>
2020-11-18 18:36:58 +0100

    add api fetch to display search movies

--- src/Component/Movies/MoviesCard.tsx
+++ src/Component/Movies/MoviesCard.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 
-export default function MoviesCard() {
+type MoviesCardProps = {
+title: string;
+poster: string;
+release: string;
+id: string;
+}
+
+export default function MoviesCard({id, title, poster, release} : MoviesCardProps) {
   return (
     <div>
-      
+      <Link to={`moviesDetails/${id}`}>
+     <p>{title}</p>
+    <img src={poster} alt="movie poster"/>
+    <p>{release}</p>
+    </Link>
     </div>
   )
 }

8da9605f0586a8c5c0600a2abe69edec3e0db567
MaevaWolff <maeva.wolff@hetic.net>
2020-11-18 12:31:11 +0100

    add flex to layout page

--- src/Layout/PageLayout.tsx
+++ src/Layout/PageLayout.tsx
@@ -4,9 +4,17 @@ import styled from "styled-components";
 import { Header } from "../Component/index";
 
 const LayoutContainer = styled.div`
+  width: 100%;
+  min-height: 100vh;
   display: flex;
   flex-direction: column;
-  min-height: 100vh;
+`;
+
+const Main = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 type PageLayoutProps = {
@@ -18,7 +26,7 @@ export default function PageLayout({ children }: PageLayoutProps) {
     <LayoutContainer>
       <Header />
 
-      <main>{children}</main>
+      <Main>{children}</Main>
     </LayoutContainer>
   );
 }

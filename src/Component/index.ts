7033c04b06b4cce202ff8456a976abb6b4a339dd
Nstampfli <nathan.stampfli@hetic.net>
2020-11-18 18:37:56 +0100

    delete useless file

--- src/Component/index.ts
+++ src/Component/index.ts
@@ -1,5 +1,4 @@
 export { default as Header } from "./Header";
 export { default as Button } from "./Button";
 export { default as InputComponent } from "./InputComponent";
-export { default as MoviesList } from "./Movies/MoviesList";
 export { default as MoviesCard } from "./Movies/MoviesCard";
